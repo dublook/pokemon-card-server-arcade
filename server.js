59d88599cc3ab006ab95ed8aa65ccc1f48c31a5f
dublook <kota.futami@gmail.com>
2017-01-08 00:17:04 +0900

    Set up server

---
+++ server.js
@@ -0,0 +1,14 @@
+{
+  "name": "pokemon-card-server-arcade",
+  "version": "0.1.0",
+  "description": "providing way to start game with different users",
+  "main": "server.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "dublook",
+  "license": "MIT",
+  "dependencies": {
+    "websocket.io": "~0.2.1"
+  }
+}
