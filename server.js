5af20419496db545a616d2deb937c9b9f8f5943f
dublook <kota.futami@gmail.com>
2017-01-08 00:27:04 +0900

    Fix server.js

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-{
-  "name": "pokemon-card-server-arcade",
-  "version": "0.1.0",
-  "description": "providing way to start game with different users",
-  "main": "server.js",
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1"
-  },
-  "author": "dublook",
-  "license": "MIT",
-  "dependencies": {
-    "websocket.io": "~0.2.1"
-  }
-}
+var ws = require('websocket.io');
+var server = ws.listen(process.env.PORT || 8124);
+
+server.on('connection', function(socket) {
+    console.log('Connection started');
+    console.log(socket.constructor);
+
+    socket.on('message', function(data) {
+        console.log('Message received:' + data);
+
+        server.clients.forEach(function(client) {
+            try {
+                client && client.send(data);
+            } catch (err) {
+                console.log(err);
+            }
+        });
+    });
+});
